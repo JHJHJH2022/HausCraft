f2506467dfcc8ba6eb159b9156486d8057860d21
Jinghan0505 <jinghanjinghan233@gmail.com>
2023-02-08 13:58:27 +0800

    Update useDragObjects.jsx

--- src/helpers/useDragObjects.jsx
+++ src/helpers/useDragObjects.jsx
@@ -22,10 +22,8 @@ export const useDragObjects = (pos, rotation) => {
           Math.floor(planeIntersectPoint.z) + 0.5,
         ]; /* to snap to grid */
 
-        setPos(newPos);
-
         setPositioned(true); // so that object stops floating after being positioned
-
+        setPos(newPos);
         // update position
         const newPosArray = [newPos[0], newPos[1], newPos[2]];
         updatebuildings(index, newPosArray);
